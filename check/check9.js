37f161a98328f8296faab003cac50c8fd990a275
hcm083214 <13723742351@163.com>
2022-06-19 13:23:45 +0800

    挑战9完成

---
+++ check/check9.js
@@ -0,0 +1,44 @@
+const puppeteer = require("puppeteer");
+try {
+    (async function () {
+        // const browser = await puppeteer.launch({
+        //     args: ["--no-sandbox", "--disable-setuid-sandbox"],
+        // });
+        // 本地测试
+        const browser = await puppeteer.launch({
+            headless: false
+        });
+        const page = await browser.newPage();
+        await page.setViewport({ width: 1200, height: 750 });
+        // await page.goto("http://127.0.0.1:8080/test9", {
+        //     waitUntil: "networkidle0", //不在有网络连接时候触发
+        // });
+
+        // 本地测试
+        await page.goto("http://127.0.0.1:5500/test9/", {
+            waitUntil: "networkidle0", //不在有网络连接时候触发
+        });
+        const logResult = ['fn1','fn2','fn3','end fn2','end fn1'];
+        function isResultExpecting(expect, result) {
+            let isPass = false
+            if (expect == result) {
+                console.log(`第${i}次符合预期`)
+                isPass = true;
+            } else {
+                console.log('测试不通过', expect, result);
+                isPass = false;
+                process.exit(1);
+            }
+            return isPass;
+        }
+        await page.on('console', (msg) => {
+            for (let i = 0; i < msg.args().length; ++i) {
+                let result = msg.args()[i]._remoteObject.value;
+                isResultExpecting(logResult[i], result);
+            }
+        })
+        await console.log(logResult)
+    })();
+} catch (error) {
+
+}
---
+++ test9/main.js
@@ -0,0 +1,51 @@
+async function fn1(next) {
+    console.log("fn1");
+    await next();
+    console.log("end fn1");
+}
+async function fn2(next) {
+    console.log("fn2");
+    await delay();
+    await next();
+    console.log("end fn2");
+}
+
+function fn3(next) {
+    console.log("fn3");
+}
+
+function delay() {
+    return new Promise((reslove, reject) => {
+        setTimeout(() => {
+            reslove();
+        }, 2000);
+    });
+}
+
+
+
+/**
+ * @description: 依次执行中间件的函数。在执行中间件的过程中，如果 next() 立即调用下一个中间件中的函数，
+ *               等下一个函数执行完后再执行前一个函数中 next 后面的代码 
+ * @param {Array} middleware 中间件，数组，保存需要执行的函数
+ */
+function compose(middleware) {
+    return function () {
+        const dispatch = function (i) {
+            // 获取中间件中的待执行函数
+            const middlewareFn = middleware[i];
+            // 将 next 函数作为参数传递给中间件中的待执行函数
+            return new Promise((resolve) => {
+                resolve(middlewareFn(function next() {
+                    // next 函数的作用执行下一个函数
+                    return dispatch(i + 1);
+                }))
+            })
+        }
+        return dispatch(0);
+    }
+}
+
+const middleware = [fn1, fn2, fn3];
+const finalFn = compose(middleware);
+finalFn();
